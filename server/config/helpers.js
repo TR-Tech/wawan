16e33ac8347a0edf4f743ddc81ec062281cc726e
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-09-19 15:18:06 +0300

    Initial server config files

---
+++ server/config/helpers.js
@@ -0,0 +1,35 @@
+var jwt = require('jwt-simple');
+
+module.exports = {
+	errorLogger: function(error, req, res, next){
+		console.log(error.stack);
+		next(error);
+	},
+
+	errorHandler: function(error, req, res, next){
+		res.send(500, {error: error.message});
+	},
+
+	decode: function(req, res, next){
+		var token = req.headers['x-access-token'];
+		var user;
+
+		if(!token){ 
+			return res.send(403);
+		}
+		try{
+			user = jwt.decode(token, 'secret');
+			req.user = user;
+			next();
+		} catch(error) {
+			return next(error);
+		}
+	},
+	repsonseHandler: function(error, req, res, body, next) {
+		if (error) {
+			next(error, req, res);
+		} else {
+			res.status(body.status).send(body.returnObj);
+		}
+	}
+};
