eb078cc73834dc1239de3060449101c29eee121e
rana1926 <rana.kelani@gmail.com>
2016-09-24 22:12:07 +0300

    referee, club and coach controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,27 @@
 var helpers = require('./helpers.js');
+
 var Competition = require('../competitions/competitionController.js');
 var Championship = require('../championships/championshipController.js');
 var Player = require('../players/playerController.js');
 
+var club =  require('../clubs/clubController.js');
+var referee = require('../referees/refereeController.js');
+var coach = require('../coaches/coachController.js');
+
 module.exports = function(app, express) {
+	//club routes
+	app.get('/api/clubs', club.getAllClubs);
+	app.get('/api/club/:id', club.getClub);
+	app.post('/api/club', club.createClub);
+	app.put('/api/club/:id/edit', club.editClub);
+	app.delete('/api/club/removeClub/:id', club.removeClub);
+
+	//referee routes
+	app.get('/api/referees', referee.getAllReferees);
+	app.get('/api/referee/:id', referee.getReferee);
+	app.post('/api/referee', referee.createReferee);
+	app.put('/api/referee/:id/edit', referee.editReferee);
+	app.delete('/api/referee/removeReferee/:id', referee.removeReferee);
 
 	// Competition routes :
 	app.get("/api/competition/:id", Competition.getOne);
@@ -28,6 +46,13 @@ module.exports = function(app, express) {
 	app.post("/api/player/createNewPlayer", Player.createNewPlayer);
 	app.post("/api/player/addImagesToPlayer/:id", Player.addImagesToPlayer);
 	app.post("/api/player/editPlayer/:id", Player.editPlayer);
+
+	//coach routes
+	app.get('/api/coachs', coach.getAllCoaches);
+	app.get('/api/coach/:id', coach.getCoach);
+	app.post('/api/coach', coach.createCoach);
+	app.put('/api/coach/:id/edit', coach.editCoach);
+	app.delete('/api/coach/removeCoach/:id', coach.removeCoach);
 
 	//error handling
 	app.use(helpers.errorLogger);
