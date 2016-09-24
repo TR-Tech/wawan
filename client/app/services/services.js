46dd9ea30716a1a5fdbbe5bb160466699fa46569
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-09-24 23:58:41 +0300

    Add player factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -74,4 +74,29 @@ angular.module('Wawan.services',[])
 			})
 		}
 	}
-} )
+})
+.factory('Imugur', function () {
+	return {
+		uploadToIMGUR : function(client_id, imgData, callback) {
+
+		$.ajax({
+			url: 'https://api.imgur.com/3/image',
+			headers: {
+				'Authorization': 'Client-ID ' + client_id,
+				'Accept': 'application/json'
+			},
+			type: 'POST',
+			data: {
+				'image': imgData,
+				'type': 'base64'
+			},
+			success: function success(res) {
+
+				if (callback) {
+					callback(res.data);
+				}
+			}
+		});
+	}
+	}
+})
