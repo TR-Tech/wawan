db31fa0640517f039b09501c92d4ea529b46b8a9
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-10-15 00:27:20 +0300

    Add JoinMethod in service

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -54,11 +54,176 @@ angular.module('Wawan.services',[])
 			return $http({
 				url : "/api/player/editPlayer/"+playerId,
 				method : "POST",
-				data : {player: player}
+				data : player
 			})
 			.then(function (res) {
 				return res.data;
 			})
+		}
+	}
+})
+.factory('Competition', function ($http) {
+	return {
+		getOne : function (competitionId) {
+			return $http({
+				url : "/api/competition/"+competitionId,
+				method : "GET"
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		createCompetition : function (competition) {
+			return $http({
+				url : "/api/competition/newcompetition",
+				method : "POST",
+				data : competition
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		getAllByChampionshipId : function (competitionId) {
+			return $http({
+				url : "/api/competition/championship/"+competitionId,
+				method : "GET"
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		getAllPlayerOfCopmetition : function (competitionId) {
+			return $http({
+				url : "/api/competition/getAllPlayerOfCopmetition/"+competitionId,
+				method : "GET"
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		addPlayerToCometition : function (competitionId,playerId) {
+			return $http({
+				url : "/api/competition/addplayer/"+competitionId,
+				method : "POST",
+				data : {playerId : playerId}
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		playerJoinCompetition : function (competitionId, playerId, flag,competitionsEnterpoint) {
+			return $http({
+				url : "/api/competition/playerJoinCompetition/"+competitionId,
+				method : "POST",
+				data : {playerId : playerId, flag : flag, competitionsEnterpoint: competitionsEnterpoint}
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		}
+
+	}
+})
+.factory('Championship', function ($http) {
+	return {
+		getOne : function (championshipId) {
+			return $http({
+				url : "/api/championship/"+championshipId,
+				method : "GET"
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		getAllChampionship : function () {
+			return $http({
+				url : "/api/championships",
+				method : "GET"
+			})
+			.then(function (res) {
+				return res.data
+			})
+		},
+		createChampionship : function (championship) {
+			return $http({
+				url : "/api/newchampionship",
+				method : "POST",
+				data : championship
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		getAllAboutChampionship : function (championshipId) {
+			return $http({
+				url : "/api/championship/getAllAboutChampionship/"+championshipId,
+				method : "GET"
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		getAllAboutChampionshipByName : function (championshipName) {
+			return $http({
+				url : "/api/championship/getAllAboutChampionshipByName/"+championshipName,
+				method : "GET"
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		addCompetitionToChampionship : function (championshipId, competitionId) {
+			return $http({
+				url : "/api/addCompetitionToChampionship/"+championshipId,
+				method : "POST",
+				data : {competitionId : competitionId}
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		addNewRefereeToChampionship : function (championshipId, refereeId) {
+			return $http({
+				url : "/api/addNewRefereeToChampionshipJoin/"+championshipId,
+				method : "POST",
+				data : {refereeId : refereeId}
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		},
+		refereeJoinChampionship : function (championshipId, refereeId, flag, enterPoints) {
+			return $http({
+				url : "/api/refereeJoinChampionship/"+championshipId,
+				method : "POST",
+				data : {refereeId : refereeId, flag : flag, enterPoints : enterPoints}
+			})
+			.then(function (res) {
+				return res.data;
+			})
+		}
+	}
+})
+.factory('Translate',function () {
+	return {
+		getArr : function (word) {
+			if(word === "Under 171 CM")
+				return "تحت 171 سم";
+			else if(word === "Under 176 CM")
+				return "تحت 176 سم";
+			else if(word === "Above 176 CM")
+				return "فوق 176 سم";
+			else if(word === "Under 75 KG")
+				return "تحت 75 كغ";
+			else if(word === "Under 85 KG")
+				return "تحت 85 كغ";
+			else if(word === "Above 85 KG")
+				return "فوق 85 كغ";	
+			else if(word === "Physic")
+				return "فيزيك";
+			else if(word === "Bodybuilding")
+				return "كمال اجسام";
+			return "No Translate";
 		}
 	}
 })
