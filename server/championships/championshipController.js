f8a032f6f9acc709675634b7b9286f0074e43625
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-10-15 00:28:59 +0300

    Implement join referees Method

--- server/championships/championshipController.js
+++ server/championships/championshipController.js
@@ -28,12 +28,16 @@ module.exports = {
 			pic : championship.pic,
 			date : championship.date,
 			awards : championship.awards,
+			awardsAr : championship.awardsAr,
 			address : championship.address,
+			addressAr : championship.addressAr,
 			requiredPoints : championship.requiredPoints,
+			winersPoints : championship.winersPoints,
 			playersLink : championship.playersLink,
 			audiencelink : championship.audiencelink,
+			refereesEnterPoint : championship.refereesEnterPoint,
 			competitions : championship.competitions,
-			referees : championship.referees
+			//referees : championship.referees
 		})
 		.save(function (err, newChampionship) {
 			console.log(newChampionship);
@@ -85,6 +89,45 @@ module.exports = {
 		})
 	},
 
+	getAllAboutChampionshipByName : function (req, res, next) {
+		var championshipObj = {};
+		var competitionArr = [];
+		var refereeArr = [];
+		var flagArr = [];
+		var championshipName = req.params.id.replace("."," ");
+		Championship.findOne({name: championshipName})
+		.exec(function (err, championship) {
+			championshipObj = championship;
+			if(championship.competitions.length > 0 || championship.joinReferees.length > 0){
+				for (var i = 0; i < championship.competitions.length; i++) {
+					Competition.findOne({_id: championship.competitions[i]})
+					.exec(function (err, competition) {
+						competitionArr.push(competition);
+						if(competitionArr.length === championship.competitions.length && refereeArr.length === championship.joinReferees.length){
+							repsonseHandler(err, req, res, {status: 200, returnObj: {championship:championshipObj,competitions:competitionArr,referees:refereeArr}}, next);
+						}
+					})
+				}
+				for (var i = 0; i < championship.joinReferees.length; i++) {
+					flagArr.push(championship.joinReferees[i].join);
+					Referee.findOne({_id : championship.joinReferees[i].referee})
+					.exec(function (err, referee) {
+						refereeArr.push(referee);
+						if(refereeArr.length === championship.joinReferees.length && competitionArr.length === championship.competitions.length){
+							for (var i = 0; i < refereeArr.length; i++) {
+								refereeArr[i].flag = flagArr[i];
+							}
+							repsonseHandler(err, req, res, {status: 200, returnObj: {championship:championshipObj,competitions:competitionArr,referees:refereeArr}}, next);
+						}
+					})
+				}
+			}
+			else{
+				repsonseHandler(err, req, res, {status: 200, returnObj: {championship:championshipObj,competitions:competitionArr,referees:refereeArr}}, next);
+			}
+		})
+	},
+
 	addCompetitionToChampionship : function (req, res, next) {
 		var championshipId = req.params.id;
 		var competitionId = req.body.competitionId;
@@ -114,5 +157,39 @@ module.exports = {
 		.exec(function (err, championship) {
 			repsonseHandler(err, req, res, {status: 200, returnObj: championship}, next);
 		})
+	},
+
+	addNewRefereeToChampionshipJoin : function (req, res, next) {
+		var championshipId = req.params.id;
+		var refereeId = req.body.refereeId;
+		var enterPoints = req.body.enterPoints;
+
+		Referee.findOneAndUpdate({_id: refereeId},{$pull : {championships : championshipId}}).exec();
+		Referee.findOneAndUpdate({_id: refereeId},{$push : {championships : championshipId}}).exec();
+		//Referee.findOneAndUpdate({_id: refereeId},{$inc : {points : enterPoints}}).exec();
+
+		Championship.findOneAndUpdate({_id: championshipId}, {$pull : {joinReferees: {referee : refereeId, join : false}}}).exec();
+		Championship.findOneAndUpdate({_id: championshipId}, {$push : {joinReferees: {referee : refereeId, join : false}}},{ new : true})
+		.exec(function (err, championship) {
+			repsonseHandler(err, req, res, {status: 200, returnObj: championship}, next);
+		})
+	},
+
+	refereeJoinChampionship : function (req, res, next) {
+		var championshipId = req.params.id;
+		var refereeId = req.body.refereeId;
+		var flag = req.body.flag;
+		var enterPoints = req.body.enterPoints;
+		console.log(enterPoints);
+		if(flag)
+			Referee.findOneAndUpdate({_id: refereeId},{$inc : {points : enterPoints}}).exec();
+		else
+			Referee.findOneAndUpdate({_id: refereeId},{$inc : {points : -enterPoints}}).exec();
+
+		Championship.findOneAndUpdate({_id: championshipId, "joinReferees.referee" : refereeId}, {$set : {"joinReferees.$.join" : flag}},{ new : true})
+		.exec(function (err, championship) {
+			repsonseHandler(err, req, res, {status: 200, returnObj: championship}, next);
+		})	
 	}
+
 }
