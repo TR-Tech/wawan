30d627528c6898f1ebbf08102a500da60b3a7aef
rana1926 <rana.1926@hotmail.com>
2017-04-19 23:15:17 +0300

    Coach page

--- client/app/coaches/coach.js
+++ client/app/coaches/coach.js
@@ -1,7 +1,32 @@
 angular.module('Wawan.coach', [])
-.controller('coachController', function($scope, $routeParams) {
+.controller('coachController', function($scope, $routeParams, $location, Player, $location, Club, Coach, Competition, Championship) {
+	$scope.data={};
+	$scope.players=[];
+	$scope.hasPlayers = false;
 	$scope.initialize = function () {
-		console.log("here is coach")
 	}
 	$scope.initialize();
+	Coach.getOneCoach($routeParams.id)
+	.then(function(coach){
+		$scope.data=coach;
+		if($scope.data.players.length>0){
+			$scope.hasPlayers = true;
+		}
+		$('.flag').addClass('flag-'+$scope.data.nationality.toLowerCase().substring(0,2));//add class to get flag
+		$scope.nationality = coach.nationality.substring(3);
+	})
+	.then(function(){
+		Club.getOneClub($scope.data.club).then(function(club){
+			$scope.club=club.name;
+		});
+		for (var i = 0; i < $scope.data.players.length; i++) {
+			Player.getOnePlayer($scope.data.players[i]).then(function(player){
+				$scope.players.push(player)	
+			})
+		}
+	});
+
+	$scope.showItem = function (type, obj) {//link to item's page
+		$location.path("/"+type+"/"+obj._id);
+	}
 });
