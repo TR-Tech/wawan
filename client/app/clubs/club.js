c0d4dcc8c5f05509a0a23edfb3cb3949cca32814
rana1926 <rana.1926@hotmail.com>
2017-04-20 19:29:47 +0300

    Add club page

--- client/app/clubs/club.js
+++ client/app/clubs/club.js
@@ -1,7 +1,35 @@
 angular.module('Wawan.club', [])
-.controller('clubController', function($scope, $routeParams) {
+.controller('clubController', function($scope, $routeParams, $location, Club, Player, Championship) {
+	$scope.data={};
+	$scope.players=[];
+	$scope.championships=[];
+	
 	$scope.initialize = function () {
-		console.log("here is club")
 	}
 	$scope.initialize();
+
+	Club.getOneClub($routeParams.id)
+	.then(function(club){
+		$scope.data=club;
+	})
+
+	.then(function(){
+		for (var i = 0; i < $scope.data.players.length; i++) {
+			Player.getOnePlayer($scope.data.players[i]).then(function(player){
+				$scope.players.push(player)	
+			})
+		}
+		for (var i = 0; i < $scope.data.championships.length; i++) {
+			Championship.getOne($scope.data.championships[i]).then(function(champ){
+				$scope.championships.push(champ)
+			})
+		}
+
+	})
+
+
+	$scope.showItem = function (type, obj) {//link to item's page
+	$location.path("/"+type+"/"+obj._id);
+}
+
 });
